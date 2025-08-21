e74c2f199b282a701a81e56bd4666ecc52ba1158
KrishJain45 <krishjain4683@gmail.com>
2025-08-21 15:37:04 +0530

    Fix TS type for BadgeButton children

--- src/components/ui/badge-button.tsx
+++ src/components/ui/badge-button.tsx
@@ -1,18 +1,22 @@
-import React from "react"
-import { IceCreamCone, SparklesIcon } from "lucide-react"
-
+import React, { ReactNode } from "react"
+import { IceCreamCone } from "lucide-react"
 import { Badge } from "@/components/ui/badge"
 
-const BadgeButton = ({children}) => {
+type BadgeButtonProps = {
+  children: ReactNode
+}
+
+const BadgeButton = ({ children }: BadgeButtonProps) => {
   return (
     <Badge
       variant="outline"
-      className="mb-3 cursor-pointer rounded-[14px] border  md:left-6"
+      className="mb-3 cursor-pointer rounded-[14px] border md:left-6"
     >
-      <IceCreamCone className=" fill-[#9ff01d] stroke-1 " />{" "}
+      <IceCreamCone className="fill-[#9ff01d] stroke-1" />{" "}
       {children}
     </Badge>
   )
 }
 
 export default BadgeButton
+
